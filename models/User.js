dde8a0c11c4fc922f04d080794ceba266e67e6dd
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-06-28 16:19:34 -0600

    updating user model from student model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+// const assignmentSchema = require('./Assignment');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true
+		},
+    email: {
+			type: String,
+			unique: true,
+			required: true,
+			match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+		},
+   
+    thoughts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Thought'
+			}
+		],
+    friends: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'User'
+			}
+		]
+  },
+  {
+    toJSON: {
+      virtuals: true
+		},
+		id: false
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+	return this.friends.length;
+});
+
+const User = model('user', UserSchema);
+
+module.exports = User;
